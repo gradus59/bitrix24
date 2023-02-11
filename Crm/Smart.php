45c1df5d7b28c2d1d0d976a936ab7d2593c5fb99
Your Name <yaaperebikovsky@1cbit.ru>
2023-02-11 15:38:43 +0500

    add smart-process class

---
+++ Crm/Smart.php
@@ -0,0 +1,148 @@
+<?php
+namespace GraDus59\Bitrix24\Crm;
+
+use Bitrix\Crm\Service\Container;
+use Bitrix\Crm\Service\Factory;
+use Bitrix\Main\Loader;
+use Bitrix\Crm\Search;
+use GraDus59\Bitrix24\Crm\Smart\Result;
+
+class Smart
+{
+    private Container $container;
+    private Factory $factory;
+    private int $id;
+
+    /**
+     * @author < GraDus59 > Перебиковский Ярослав
+     */
+    private function __construct()
+    {
+        Loader::includeModule('crm');
+        $this->container = Container::getInstance();
+    }
+
+    public static function getObject()
+    {
+        return new self();
+    }
+
+    public function classById(int $id): Smart
+    {
+        $this->id = $id;
+        $type = $this->container->getType($id);
+        $this->setFactoryByType($type);
+        return $this;
+    }
+
+    public function classByEntityTypeId(string $entityTypeId): Smart
+    {
+        $type = $this->container->getTypeByEntityTypeId($entityTypeId);
+        $this->setFactoryByType($type);
+        return $this;
+    }
+
+    public function classByCode(string $code): Smart
+    {
+        $id = $this->getTypeByCode($code)["ID"];
+        return $this->classById($id);
+    }
+
+    public function getById(int $id)
+    {
+        return $this->getBy("ID",$id);
+    }
+
+    public function getBy(string $field, $value)
+    {
+        $parameters['filter'] = [$field => $value];
+        $parameters['select'] = ["*"];
+        $parameters['order'] = ["ID"];
+        $parameters['limit'] = 1;
+        $result = $this->getList($parameters);
+
+        return $result->fetch();
+    }
+
+    public function getList(array $parameters)
+    {
+        $items = $this->getFactory()->getItems($parameters);
+        if(!$items)
+            return false;
+
+        $resultObject = Result::getInstance();
+        $resultObject->setResult($items);
+        return $resultObject;
+    }
+
+    public function add(array $data)
+    {
+        $item = $this->getFactory()->createItem();
+        $item->setFromCompatibleData($data);
+
+        $result = $item->save();
+        return $result->isSuccess() ? $result->getId() : $result->getErrorMessages();
+    }
+
+    public function update(int $id, array $data)
+    {
+        $item = $this->getFactory()->getItem($id);
+        if(!$item)
+            return false;
+
+        foreach ($data as $key => $field)
+            $item->set($key, $field);
+
+        $result = $item->save();
+        return $result->isSuccess() ?: $result->getErrorMessages();
+    }
+
+    public function delete(int $id)
+    {
+        // TODO: rest or unset or delete
+    }
+
+    public function getEntityId(): string
+    {
+        return \CCrmOwnerType::DynamicTypePrefixName . $this->id;
+    }
+
+    public function getFieldsInfo(): array
+    {
+        return $this->getFactory()->getFieldsInfo();
+    }
+
+    public function getFactory(): Factory
+    {
+        return $this->factory;
+    }
+
+    public function createIndex(int $id)
+    {
+        $search = new Search\DynamicTypeSearchContentBuilder($this->factory->getEntityTypeId());
+        $search->build($id);
+    }
+
+    public function getTypeList(array $parameters): \Bitrix\Main\ORM\Query\Result
+    {
+        $typeDataClass = $this->container->getDynamicTypeDataClass();
+        return $typeDataClass::getList($parameters);
+    }
+
+    private function setFactoryByType($type): void
+    {
+        $this->factory = $this->container->getDynamicFactoryByType($type);
+    }
+
+    private function getTypeByCode(string $code)
+    {
+        $resType = $this->getTypeList([
+            "filter" => [
+                "CODE" => $code
+            ],
+            "select" => ["ID","CODE","ENTITY_TYPE_ID"]
+        ]);
+
+        return $resType->fetch();
+    }
+}
---
+++ Crm/Src/Smart/Result.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace GraDus59\Bitrix24\Crm\Smart;
+
+class Result
+{
+    private static $result;
+    private static ?Result $instance = null;
+
+    /**
+     * @author < GraDus59 > Перебиковский Ярослав
+     */
+    final private function __construct()
+    {
+    }
+
+    public static function getInstance(): ?Result
+    {
+        if (!self::$instance)
+            self::$instance = new self();
+        return self::$instance;
+    }
+
+    public function setResult($result)
+    {
+        self::$result = $result;
+    }
+
+    public function fetch()
+    {
+        $item = array_shift(self::$result);
+        return $item->getData();
+    }
+
+    public function fetchAll(): array
+    {
+        $data = [];
+        foreach (self::$result as $item)
+            $data[] = $item->getData();
+
+        return $data;
+    }
+}
