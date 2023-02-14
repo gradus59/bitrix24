0d8a5b6610cab618453e64d9a74f8db8cd7658ac
Your Name <yaaperebikovsky@1cbit.ru>
2023-02-14 23:38:21 +0500

    (activity): call upd

--- Crm/Activity/Call.php
+++ Crm/Activity/Call.php
@@ -4,26 +4,61 @@ namespace GraDus59\Bitrix24\Crm\Activity;
 
 class Call
 {
+    private ?Planer $planer = null;
+    private $createdBy;
+
     public function __construct()
     {
         \CModule::IncludeModule("crm");
         \CBitrixComponent::includeComponentClass('bitrix:crm.activity.planner');
     }
 
-    public function add(
+    public function setHead(
         string $subject,
         string $description,
         int $createdBy,
-        int $responsibleId,
-        string $direction = "",
-        array  $notify = [],
-        array  $date = [],
-        array  $duration = [],
-        int $dealId = 0,
-        bool   $completed = false,
-        bool   $important = false,
-        array $communications = []
+        int $responsibleId
     )
+    {
+        $this->planer = new Planer();
+        $this->planer->setTitle($subject,$description);
+        $this->planer->setResponsible($responsibleId);
+        $this->createdBy = $createdBy;
+        $this->planer->setType();
+        $this->planer->setProvider();
+    }
+
+    public function callCompleted()
+    {
+        $this->planer->setCompleted(true);
+    }
+
+    public function callImportant()
+    {
+        $this->planer->setImportant(true);
+    }
+
+    public function setNotify(array $notify)
+    {
+        $notifyValue = array_key_exists('VALUE',$notify) ? $notify['VALUE'] : 60;
+        $notifyType = array_key_exists('TYPE',$notify) ? $notify['TYPE'] : Planer::MINUTE_TEXT;
+
+        $this->planer->setNotify($notifyValue,$notifyType);
+    }
+
+    public function setDirection(string $direction)
+    {
+        $this->planer->setDirection($direction);
+    }
+
+    public function setRelations(int $dealId = 0,array $communications = [])
+    {
+        if(array_key_exists('ID',$communications))
+            $this->planer->setCommunications($communications['ENTITY_NAME'],$communications['ID']);
+        $this->planer->setDeal($dealId);
+    }
+
+    public function add(array  $date = [], array  $duration = [])
     {
         $start = count($date['START']) == 0 ? date('d.m.Y H:i:s') : $date['START'];
         $end = count($date['END']) == 0 ? date('d.m.Y H:i:s') : $date['END'];
@@ -32,27 +67,12 @@ class Call
         $durationValue = array_key_exists('VALUE',$duration) ? $duration['VALUE'] : 60;
         $durationType = array_key_exists('TYPE',$duration) ? $duration['TYPE'] : Planer::MINUTE_TEXT;
 
-        $notifyValue = array_key_exists('VALUE',$notify) ? $notify['VALUE'] : 60;
-        $notifyType = array_key_exists('TYPE',$notify) ? $notify['TYPE'] : Planer::MINUTE_TEXT;
+        $this->planer->setDates($start,$end,$toUser);
+        $this->planer->setDuration($durationValue,$durationType);
 
-        $planer = new Planer();
-        $planer->setCompleted($completed);
-        $planer->setImportant($important);
-        $planer->setType();
-        $planer->setProvider();
-        $planer->setDirection($direction);
-        $planer->setDates($start,$end,$toUser);
-        $planer->setNotify($notifyValue,$notifyType);
-        $planer->setDuration($durationValue,$durationType);
-        $planer->setTitle($subject,$description);
-        if(array_key_exists('ID',$communications))
-            $planer->setCommunications($communications['ENTITY_NAME'],$communications['ID']);
-        $planer->setResponsible($responsibleId);
-        $planer->setDeal($dealId);
-        $planer->setDefault();
+        $this->planer->setDefault();
+        $data = $this->planer->getData();
 
-        $data = $planer->getData();
-
-        return \CrmActivityPlannerComponent::saveActivity($data,$createdBy,SITE_ID);
+        return \CrmActivityPlannerComponent::saveActivity($data,$this->createdBy,SITE_ID);
     }
 }
---
+++ Crm/Activity/README.md
@@ -0,0 +1,73 @@
+# Как использовать классы пространства Activity
+
+## Planer
+
+#### Константы
+
+- Planer::DIRECTION_INCOMING - Исходящий звонок
+- Planer::DIRECTION_OUTGOING - Входящий звонок
+- Planer::MINUTE_TEXT - Минуты
+- Planer::HOURS_TEXT - Часы
+- Planer::DAYS_TEXT - Дни
+
+## Call
+
+Инициализация классадля создания звонка
+```
+use GraDus59\Bitrix24\Crm\Activity\Call;
+use GraDus59\Bitrix24\Crm\Activity\Planer;
+
+$call = new Call();
+```
+
+Установка заголовка звонка
+- Заголовок
+- Описание
+- От кого создаем
+- Ответственный
+```
+$call->setHead('subject','description',1,1);
+```
+
+Установка выполненного звонка
+```
+$call->callCompleted();
+```
+
+Установка важности звонка
+```
+$call->callImportant();
+```
+
+Установка напоминания звонка
+```
+$notify = [ "VALUE" => 15, "TYPE" => Planer::MINUTE_TEXT ];
+$call->setNotify($notify);
+```
+
+Установка типа звонка
+```
+$call->setDirection(Planer::DIRECTION_INCOMING);
+```
+
+Установка "С кем" с привязкой к компании или контакту
+```
+$call->setRelations(123);
+$whith = [ "ENTITY_NAME" => \CCrmOwnerType::CompanyName, "ID" => 753 ];
+$call->setRelations(123,$whith);
+```
+- Дата от и до, если не указать, встанет сейчас
+- TO_USER если установлена то время будет не по серверу передано, а по настройкам из профиля
+- Детальное указание диапазона
+```
+$call->add(
+    [
+        "START" => "", 
+        "END" => ""
+    ],
+    [
+        "VALUE" => 15, 
+        "TYPE" => Planer::MINUTE_TEXT
+    ]
+);
+```
